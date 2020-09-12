76eb8d5a1162f099092b95c51b5ba5b03757d29b
Elvin <64169101+elvinyeka@users.noreply.github.com>
2020-09-12 16:36:58 +0400

    Update bookstore.py

--- bookstore.py
+++ bookstore.py
@@ -1,10 +1,4 @@
-import json
-i = 1
-bookList = []
 
-kitabSayi = int(input("Kitabların sayını daxil edin: "))
-
-while i<= kitabSayi:
 import json
 
 i = 1
@@ -43,38 +37,6 @@ while True:
 for _book in bookList:
     print(
         f" Kitabın Adı: {_book['bookName']}\n Kitabın müəllifi: {_book['bookAuthor']}\n Kitabın qiyməti: {_book['bookAmount']} AZN\n Kitabın səhifə sayı: {_book['bookPage']}\n Kitabın janrı: {_book['bookGenre']}\n Kitabın sayı: {_book['bookNumber']}")
-
-    with open("booklist.json", "w") as connect:
-        json.dump(bookList, connect)
-
-    print(f"{i}-ci kitabın məlumatlarını daxil edin: ")
-    i+=1
-    _bookName = input("Kitabın Adı: ")
-    _bookAuthor = input("Müəllifi: ")
-    _bookAmount = int(input("Kitabın qiyməti: "))
-    _bookPage = int(input("Kitabın səhifə sayı: "))
-    _bookGenre = input("Kitabın janrı: ")
-    _bookNumber = int(input("Bu kitabın sayını daxil edin:"))
-    book = {
-        'bookName': _bookName,
-        'bookAuthor': _bookAuthor,
-        'bookAmount': _bookAmount,
-        'bookPage': _bookPage,
-        'bookGenre': _bookGenre,
-        'bookNumber': _bookNumber
-    }
-
-    bookList.append(book)
-    Davam = int(input("Novbəti kitabın məlumatlarını daxil etmek isteyirsinizse 1 yazin\nSonlamaq isteyirsizse 2 yazin :"))
-    if Davam == 1:
-        continue
-    else:
-        break
-
-for _book in bookList:
-    print(f" Kitabın Adı: {_book['bookName']}\n Kitabın müəllifi: {_book['bookAuthor']}\n Kitabın qiyməti: {_book['bookAmount']} AZN\n Kitabın səhifə sayı: {_book['bookPage']}\n Kitabın janrı: {_book['bookGenre']}\n Kitabın sayı: {_book['bookNumber']}")
-
-
 
     with open("booklist.json", "w") as connect:
         json.dump(bookList, connect)
